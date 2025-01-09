ee073312fdb334934f9c10c7151e71c3f5113660
Heimer Nguyen <heimer@vinova.com.sg>
2025-01-09 16:50:20 +0700

    feat: implement user module with CRUD operations and basic user DTOs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express/interfaces';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.setViewEngine('ejs');
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.useStaticAssets(join(__dirname, '..', 'public'));
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
